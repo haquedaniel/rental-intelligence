b4628f8314f406b7a2ffe5f6836b10a698a598c0
Daniel Haque <daniel@Daniels-MacBook-Pro.local>
2026-06-21 15:58:35 -0400

    shortcuts

--- apps/cleaner-web/app/owner/cockpit/page.tsx
+++ apps/cleaner-web/app/owner/cockpit/page.tsx
@@ -1,5 +1,15 @@
 export const dynamic = "force-dynamic";
 
+export const metadata = {
+  title: "Pylotis Owner",
+  manifest: "/owner/cockpit/manifest.webmanifest",
+  appleWebApp: {
+    capable: true,
+    title: "Pylotis",
+    statusBarStyle: "default",
+  },
+};
+
 import Page from "../../admin/planning-v2/page";
 
 export default Page;
